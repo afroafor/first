a0ec23ac34456be53a092dbf34907af995155c02
afroafor <150746712+afroafor@users.noreply.github.com>
2025-04-17 01:18:26 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function handleclickContactMe(){
+    window.open("https://www.google.com/search?gs_ssp=eJzj4tTP1TcwMU02T1JgNGB0YPBiS8_PT89JBQBASQXT&q=google&rlz=1C1CHZN_enGH1152GH1152&oq=go&gs_lcrp=EgZjaHJvbWUqEwgBEC4YgwEYxwEYsQMY0QMYgAQyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIMCAIQIxgnGIAEGIoFMgYIAxBFGDwyBggEEEUYPDIGCAUQRRg8MgYIBhBFGDwyBggHEEUYPNIBCDI4OThqMWo0qAIAsAIB&sourceid=chrome&ie=UTF-8");
+}
